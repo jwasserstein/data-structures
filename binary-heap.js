ef316c50022d689347d0413a92bb0d0378c231ca
justinwasserstein <justin.wasserstein@gmail.com>
2021-03-13 16:40:14 -0500

    Add MinHeap to binary-heap.js

--- binary-heap.js
+++ binary-heap.js
@@ -1,49 +1,63 @@
-class Node {
-    constructor(value){
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
-}
-
-class MaxBinaryHeap {
+class MinHeap {
     constructor(){
-        this.root = null;
-        this.lastParent = this.root;
+        this.data = [];
     }
 
-    insert(value){
-        const newNode = new Node(value);
-        
-        if(this.root === null){
-            this.root = newNode;
-            this.lastParent = this.root;
-            return this;
+    static getParentIdx(childIdx){
+        if(childIdx === 0) return -1;
+        return childIdx % 2 === 0 ? (childIdx-2)/2 : (childIdx-1)/2;
+    }
+
+    static getLeftChildIdx(parentIdx){
+        return parentIdx*2+1;
+    }
+
+    static getRightChildIdx(parentIdx){
+        return parentIdx*2+2;
+    }
+
+    add(val){
+        this.data.push(val);
+        let idx = this.data.length-1;
+        let pIdx = MinHeap.getParentIdx(idx);
+        while(pIdx !== -1 && this.data[idx] < this.data[pIdx]){
+            [this.data[idx], this.data[pIdx]] = [this.data[pIdx], this.data[idx]];
+            idx = pIdx;
+            pIdx = MinHeap.getParentIdx(idx);
+        }
+    }
+
+    peek(){
+        return this.data[0];
+    }
+
+    remove(){
+        if(this.data.length === 1) return this.data.pop();
+
+        const val = this.data[0];
+        [this.data[0], this.data[this.data.length-1]] = [this.data[this.data.length-1], this.data[0]];
+        this.data.pop();
+
+        let idx = 0;
+        let leftChildIdx = MinHeap.getLeftChildIdx(idx);
+        let rightChildIdx = MinHeap.getRightChildIdx(idx);
+        while((leftChildIdx !== -1 && this.data[idx] > this.data[leftChildIdx]) || (rightChildIdx !== -1 && this.data[idx] > this.data[rightChildIdx])){
+            let childIdx = this.data[leftChildIdx] < this.data[rightChildIdx] ? leftChildIdx : rightChildIdx;
+            [this.data[idx], this.data[childIdx]] = [this.data[childIdx], this.data[idx]];
+            idx = childIdx;
+            leftChildIdx = MinHeap.getLeftChildIdx(idx);
+            rightChildIdx = MinHeap.getRightChildIdx(idx);
         }
 
-        if(!this.lastParent.left){
-            this.lastParent.left = newNode;
-            return this;
-        }
-
-        if(!this.lastParent.right){
-            this.lastParent.right = newNode;
-            this.lastParent = this.lastParent.left;
-            return this;
-        }
+        return val;
     }
 }
 
-const h = new MaxBinaryHeap();
-h.insert(15);
-h.insert(7);
-h.insert(9);
-h.insert(3);
-h.insert(5);
-debugger;
+const heap = new MinHeap();
 
-//      15          
-//     /  \
-//    7    9
-//   / \    
-//  3   5   
+heap.add(5);
+heap.add(3);
+heap.add(7);
+heap.add(1);
+heap.add(2);
+heap.add(9);
