20f132405accce76bdf88f3adf3b929f7d6465c9
justinwasserstein <justin.wasserstein@gmail.com>
2021-03-14 11:36:28 -0400

    Add MaxHeap to binary-heap.js

--- binary-heap.js
+++ binary-heap.js
@@ -61,3 +61,71 @@ heap.add(7);
 heap.add(1);
 heap.add(2);
 heap.add(9);
+
+
+class MaxHeap {
+    constructor(arr){
+        this.data = arr.slice();
+        const lastNonLeafIdx = MaxHeap.getParentIdx(this.data.length-1);
+        for(let i = lastNonLeafIdx; i >= 0; i--){
+            this._heapifyDown(i);
+        }
+    }
+
+    static getParentIdx(childIdx){
+        return Math.floor((childIdx - 1)/2);
+    }
+
+    static getLeftChildIdx(parentIdx){
+        return parentIdx*2+1;
+    }
+
+    static getRightChildIdx(parentIdx){
+        return parentIdx*2+2;
+    }
+
+    _heapifyDown(idx){
+        let leftChildIdx = MaxHeap.getLeftChildIdx(idx);
+        let rightChildIdx = MaxHeap.getRightChildIdx(idx);
+        if(leftChildIdx >= this.data.length) leftChildIdx = -1;
+        if(rightChildIdx >= this.data.length) rightChildIdx = -1;
+
+        while((leftChildIdx !== -1 && this.data[leftChildIdx] > this.data[idx]) || (rightChildIdx !== -1 && this.data[rightChildIdx] > this.data[idx])){
+            let childIdx = rightChildIdx === -1 || this.data[leftChildIdx] > this.data[rightChildIdx] ? leftChildIdx : rightChildIdx;
+            [this.data[idx], this.data[childIdx]] = [this.data[childIdx], this.data[idx]];
+
+            idx = childIdx;
+            leftChildIdx = MaxHeap.getLeftChildIdx(idx);
+            rightChildIdx = MaxHeap.getRightChildIdx(idx);
+            if(leftChildIdx >= this.data.length) leftChildIdx = -1;
+            if(rightChildIdx >= this.data.length) rightChildIdx = -1;
+        }
+    }
+
+    add(val){
+        this.data.push(val);
+        let idx = this.data.length-1;
+        let pIdx = MaxHeap.getParentIdx(idx);
+        while(pIdx !== -1 && this.data[idx] > this.data[pIdx]){
+            [this.data[idx], this.data[pIdx]] = [this.data[pIdx], this.data[idx]];
+            idx = pIdx;
+            pIdx = MaxHeap.getParentIdx(idx);
+        }
+    }
+
+    peek(){
+        return this.data.length > 0 ? this.data[0] : null;
+    }
+
+    remove(){
+        if(this.data.length === 0) return null;
+
+        [this.data[0], this.data[this.data.length-1]] = [this.data[this.data.length-1], this.data[0]];
+        const val = this.data.pop();
+        this._heapifyDown(0);
+        
+        return val;
+    }
+}
+
+const maxHeap = new MaxHeap([1, 5, 3, 4, 2, 1, 2, 3, 4, 5, 6, 8]);
